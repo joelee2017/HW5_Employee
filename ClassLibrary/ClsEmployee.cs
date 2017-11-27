e1f620387714a6718c5404f2ffa56e930142e9a3
joelee2017 <as955133@gmail.com>
2017-11-27 15:13:37 +0800

    class編寫

--- ClassLibrary/ClsEmployee.cs
+++ ClassLibrary/ClsEmployee.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class ClsEmployee
+    public enum Jobitle
+    {
+        Manager, Engineer, Tester
+    }
+     
+
+    class ClsEmployee : IDisposable, IComparable<ClsEmployee>
     {
 
+        public string EmpName { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public Jobitle Jobitle { get; set; }
+
+        public void Employees(string empname, DateTime hirddate, decimal salary, Jobitle jobtitle)
+        {
+            EmpName = empname;
+            HireDate = hirddate;
+            Salary = salary;
+            Jobitle = jobtitle;
+        }
+
+        public override string ToString()
+        {
+            return $"Name:{EmpName}, HireDate:{HireDate}, Salary:{Salary}, JobTitle:{Jobitle}";
+        }
+
+        public int CompareTo(ClsEmployee other)
+        {
+            return this.Salary.CompareTo(other.Salary);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
     }
 }
